a041a7579b1e1b390fc63081ac7ef2ec7d97c438
Lake <echonoshy@gmail.com>
2024-08-26 22:49:08 +0800

    feat: fastapi run server script

--- instances/scripts/run.sh
+++ instances/scripts/run.sh
@@ -4,18 +4,11 @@
 HOST=$(python config/read_config_cli.py server.host)
 PORT=$(python config/read_config_cli server.port)
 WORKERS=$(python config/read_config_cli app.workers)
-RELOAD=$(python config/read_config_cli app.reload)
 
-# 将 RELOAD 转换为相应的命令行参数
-if [ "$RELOAD" = "true" ]; then
-  RELOAD_FLAG="--reload"
-else
-  RELOAD_FLAG=""
-fi
 
 # 启动 FastAPI 服务
 echo "Starting FastAPI service on $HOST:$PORT with $WORKERS workers..."
 
-uvicorn main:app --host $HOST --port $PORT --workers $WORKERS $RELOAD_FLAG &
+nohup uvicorn main:app --host $HOST --port $PORT --workers $WORKERS &
 
 echo "FastAPI service is running on http://$HOST:$PORT"
