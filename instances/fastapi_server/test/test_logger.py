1dad7902bbe7e0795cd1268e0f3b29134c801cfc
Lake <echonoshy@gmail.com>
2024-09-06 22:02:29 +0800

    fix: log pytest

--- instances/fastapi_server/test/test_logger.py
+++ instances/fastapi_server/test/test_logger.py
@@ -8,6 +8,6 @@ sys.path.append(os.path.abspath(os.path.join(os.getcwd(), 'instances/fastapi_ser
 def test_logger():
     import logging 
     from log import logger
-    logger = logger.setup_logger(log_level="INFO")
+    logger = logger.setup_logger(log_name="CQA", log_directory="logs", log_level="INFO")
     
     assert logger.level == logging.INFO 
