18be63cb7733224c46bdde13c69ded7c3c443813
Lake <echonoshy@gmail.com>
2024-10-16 19:50:56 +0800

    pytest: logger

--- instances/fastapi_server/test/test_logger.py
+++ instances/fastapi_server/test/test_logger.py
@@ -6,8 +6,10 @@ sys.path.append(os.path.abspath(os.path.join(os.getcwd(), 'instances/fastapi_ser
 
 
 def test_logger():
-    import logging 
     from log import logger
-    logger = logger.setup_logger(log_name="CQA", log_directory="logs", log_level="INFO")
+    logger = logger.setup_logger(log_name="CQA", log_dir="logs", log_level="INFO")
     
-    assert logger.level == logging.INFO 
+    assert logger.level("INFO").name == "INFO"
+    
+    
+test_logger()
