7087f8288ce3a61837f00669ca80b5963facc0ac
Lake <56507185+echonoshy@users.noreply.github.com>
2025-01-03 10:10:10 +0800

    Update run.sh

--- instances/fastapi_server/scripts/run.sh
+++ instances/fastapi_server/scripts/run.sh
@@ -9,6 +9,16 @@ WORKERS=$(python config/read_config_cli app.workers)
 # 启动 FastAPI 服务
 echo "Starting FastAPI service on $HOST:$PORT with $WORKERS workers..."
 
-nohup uvicorn main:app --host $HOST --port $PORT --workers $WORKERS &
+# 使用uvicorn启动
+# nohup uvicorn main:app --host $HOST --port $PORT --workers $WORKERS &
+
+# gunicorn 启动更稳定
+# logger已经将stdout 输出到logger file
+nohup gunicorn main:app \
+  --bind "$HOST:$PORT" \
+  --workers $WORKERS \
+  --worker-class "uvicorn.workers.UvicornWorker" \
+  > /dev/null &
+
 
 echo "FastAPI service is running on http://$HOST:$PORT"
